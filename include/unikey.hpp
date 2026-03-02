57a1d6f244ebfe7157d122ca3c326d6c943751e5
KevJin97 <14825916+KevJin97@users.noreply.github.com>
2026-03-02 02:24:35 -0500

    Moved DBus WiFi implementation to separate header

--- include/unikey.hpp
+++ include/unikey.hpp
@@ -21,12 +21,8 @@ extern std::unique_ptr<sdbus::IObject> unikey_wifi_dbus_obj;
 
 extern void register_to_dbus();
 extern void register_device_dbus_cmds();
-extern void register_bluetooth_dbus_cmds();
-extern void register_wifi_dbus_cmds();
 extern void dbus_trigger_cmd();
 extern void dbus_set_timeout_cmd(sdbus::MethodCall);
-extern void dbus_connect_to_ip(sdbus::MethodCall);
-extern void dbus_toggle_unikey_server();
 
 // extern void broadcast_service();
 extern int change_group_permissions();
